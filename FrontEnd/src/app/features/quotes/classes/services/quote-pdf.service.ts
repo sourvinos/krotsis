f01fbdb7d11dbeac0c5a14776297d80f3211b8b6
John Sourvinos <johnsourvinos@hotmail.com>
2022-08-29 16:58:20 +0300

    Continues quote pdf

--- FrontEnd/src/app/features/quotes/classes/services/quote-pdf.service.ts
+++ FrontEnd/src/app/features/quotes/classes/services/quote-pdf.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core'
 // Custom
-import { HelperService } from 'src/app/shared/services/helper.service'
 import { Item } from '../models/item'
-import { LocalStorageService } from 'src/app/shared/services/local-storage.service'
 import { LogoService } from 'src/app/shared/services/logo.service'
 // Fonts
 import pdfFonts from 'pdfmake/build/vfs_fonts'
 import pdfMake from 'pdfmake/build/pdfmake'
 import { strBowlbyOne } from '../../../../../assets/fonts/BowlbyOne.Base64.encoded'
 import { strPFHandbookPro } from '../../../../../assets/fonts/PF-Handbook-Pro.Base64.encoded'
+import { strPFHandbookProBold } from '../../../../../assets/fonts/PF-Handbook-Pro-Bold.Base64.encoded'
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs
 
@@ -16,55 +15,55 @@ pdfMake.vfs = pdfFonts.pdfMake.vfs
 
 export class QuotePDFService {
 
-    constructor(private helperService: HelperService, private localStorageService: LocalStorageService, private logoService: LogoService) { }
+    constructor(private logoService: LogoService) { }
 
     //#region public methods
 
     public createPDF(records: Item[]): void {
         this.setFonts()
         const dd = {
-            background: this.setBackgroundImage(),
-            info: this.setPageInfo(),
+            background: this.backgroundImage(),
+            info: this.pageMetadata(),
             pageOrientation: 'portrait',
             pageSize: 'A4',
+            footer: () => {
+                return this.footer()
+            },
             content: [
                 {
                     margin: [0, 0, 0, 20],
                     columns: [
-                        // this.setLogo(),
-                        this.setOurData(),
-                        this.setCustomerData()
+                        this.ourDataAndHeader(),
+                        this.customerData()
                     ]
                 },
                 {
                     table: {
                         headerRows: 1,
                         widths: ['*', 50, 50],
-                        body: this.createLines(records),
+                        body: this.detailLines(records),
                     },
                     margin: [11, 0, 0, 0],
                     layout: 'lightHorizontalLines'
                 },
-                {
-                    canvas: [
-                        {
-                            type: 'line',
-                            x1: 11, y1: 580,
-                            x2: 515, y2: 580,
-                            lineWidth: 1
-                        }
-                    ]
-                }
+                // {
+                //     canvas: [
+                //         {
+                //             type: 'line',
+                //             x1: 11, y1: 525,
+                //             x2: 515, y2: 525,
+                //             lineWidth: 1
+                //         }
+                //     ]
+                // }
             ],
             styles: {
                 BowlbyOne: { font: 'BowlbyOne' },
-                PFHandbookPro: { font: 'PFHandbookPro' }
+                PFHandbookPro: { font: 'PFHandbookPro' },
+                PFHandbookProBold: { font: 'PFHandbookProBold' }
             },
             defaultStyle: {
                 font: 'PFHandbookPro',
-            },
-            footer: () => {
-                return this.setFooter()
             }
         }
         this.createPdf(dd)
@@ -78,33 +77,18 @@ export class QuotePDFService {
         pdfMake.createPdf(document).open()
     }
 
-    private createLines(records: Item[]): any[] {
-        const rows = []
-        rows.push([
-            { text: 'Περιγραφή', fontSize: 6, margin: [0, 0, 0, 0] },
-            { text: 'Καθαρή αξία', fontSize: 6 },
-            { text: 'Αξία με ΦΠΑ', fontSize: 6 },
-        ])
-        for (const record of records) {
-            rows.push([
-                { text: record.description, fontSize: 5 },
-                { text: record.netPrice, alignment: 'right', fontSize: 5 },
-                { text: record.grossPrice, alignment: 'right', fontSize: 5 }
-            ])
-        }
-        return rows
-    }
-
     private setFonts(): void {
         pdfFonts.pdfMake.vfs['BowlbyOne'] = strBowlbyOne
         pdfFonts.pdfMake.vfs['PFHandbookPro'] = strPFHandbookPro
+        pdfFonts.pdfMake.vfs['PFHandbookProBold'] = strPFHandbookProBold
         pdfMake.fonts = {
             BowlbyOne: { normal: 'BowlbyOne' },
-            PFHandbookPro: { normal: 'PFHandbookPro', }
+            PFHandbookPro: { normal: 'PFHandbookPro' },
+            PFHandbookProBold: { normal: 'PFHandbookProBold' }
         }
     }
 
-    private setBackgroundImage(): any[] {
+    private backgroundImage(): any[] {
         const backgroundImage = [
             {
                 image: this.logoService.getLogo(),
@@ -116,69 +100,74 @@ export class QuotePDFService {
         return backgroundImage
     }
 
-    private setPageInfo(): any {
+    private pageMetadata(): any {
         const pageInfo = {
-            title: 'Οικονομική Προσφορά'
+            title: 'ΟΙΚΟΝΟΜΙΚΗ ΠΡΟΣΦΟΡΑ'
         }
         return pageInfo
     }
 
-    private setLogo(): any {
-        const logo = {
-            type: 'none',
-            width: 60,
-            margin: [0, -6, 0, 0],
-            ul: [
-                { image: this.logoService.getLogo(), fit: [40, 40], alignment: 'left' },
-            ]
-        }
-        return logo
-    }
-
-    private setOurData(): any {
+    private ourDataAndHeader(): any {
         const title = {
             type: 'none',
             margin: [0, 0, 0, 0],
             ul: [
-                { text: 'Krotsis',  fontSize: 24, style: 'BowlbyOne' },
-                { text: 'ΕΜΠΟΡΙΟ ΕΛΑΣΤΙΚΩΝ & ΖΑΝΤΩΝ', fontSize: 12, margin: [0, -7, 0, 0] },
-                { text: 'ΟΙΚΟΝΟΜΙΚΗ ΠΡΟΣΦΟΡΑ', fontSize: 11, margin: [0, 10, 0, 0] },
-                { text: 'ΓΙΑ ΤΟ ΟΧΗΜΑ: ΚΗΙ 1234', fontSize: 9 },
+                { text: 'Krotsis', fontSize: 24, style: 'BowlbyOne' },
+                { text: 'ΕΜΠΟΡΙΟ ΕΛΑΣΤΙΚΩΝ & ΖΑΝΤΩΝ', fontSize: 12, margin: [0, -7, 0, 0], style: 'PFHandbookProBold' },
+                { text: 'ΟΙΚΟΝΟΜΙΚΗ ΠΡΟΣΦΟΡΑ', fontSize: 12, margin: [0, 25.5, 0, 0], style: 'PFHandbookProBold' },
+                { text: 'ΓΙΑ ΤΟ ΟΧΗΜΑ: ΚΗΙ 1234', fontSize: 11 }
             ]
         }
         return title
     }
 
-    private setCustomerData(): any {
+    private customerData(): any {
         const title = {
             type: 'none',
             margin: [0, 14, 0, 0],
             ul: [
-                { text: 'ΠΑΡΑΛΗΠΤΗΣ', fontSize: 10 },
-                { text: 'ΓΡΑΜΜΗ 1', fontSize: 8 },
-                { text: 'ΓΡΑΜΜΗ 2', fontSize: 8 },
-                { text: 'ΓΡΑΜΜΗ 3', fontSize: 8 },
-                { text: 'ΓΡΑΜΜΗ 4', fontSize: 8 },
-                { text: 'email@server.com', fontSize: 8 },
-                { text: 'ΤΗΛΕΦΩΝΑ: 26613 65114', fontSize: 8 }                
+                { text: 'ΠΑΡΑΛΗΠΤΗΣ', fontSize: 11, style: 'PFHandbookProBold' },
+                { text: 'ΔΗΜΟΣ ΚΕΝΤΡΙΚΗΣ ΚΕΡΚΥΡΑΣ ΚΑΙ ΔΙΑΠΟΝΤΙΩΝ ΝΗΣΩΝ', fontSize: 11 },
+                { text: 'ΔΙΕΥΘΥΝΣΗ ΚΑΘΑΡΙΟΤΗΤΑΣ ΚΑΙ ΑΝΑΚΥΚΛΩΣΗΣ', fontSize: 11 },
+                { text: 'ΤΜΗΜΑ ΔΙΑΧΕΙΡΙΣΗΣ ΚΑΙ ΣΥΝΤΗΡΗΣΗΣ ΟΧΗΜΑΤΩΝ', fontSize: 11 },
+                { text: 'ΓΡΑΦΕΙΟ ΚΙΝΗΣΗΣ', fontSize: 11 },
+                { text: 'ΠΛΗΡΟΦΟΡΙΕΣ: ZAXOY E. - ΡΑΨΟΜΑΝΙΚΗΣ Ν.', fontSize: 11 },
+                { text: 'ΤΗΛΕΦΩΝΑ: 26613 62749', fontSize: 11 }
             ]
         }
         return title
     }
 
-    private setFooter(): any {
+    private detailLines(records: Item[]): any[] {
+        const rows = []
+        rows.push([
+            { text: 'ΠΕΡΙΓΡΑΦΗ', fontSize: 11, margin: [0, 0, 0, 0] },
+            { text: 'ΚΑΘΑΡΗ ΑΞΙΑ', fontSize: 11 },
+            { text: 'ΑΞΙΑ ΜΕ ΦΠΑ', fontSize: 11 },
+        ])
+        for (const record of records) {
+            rows.push([
+                { text: record.description, fontSize: 10 },
+                { text: record.netPrice, alignment: 'right', fontSize: 10 },
+                { text: record.grossPrice, alignment: 'right', fontSize: 10 }
+            ])
+        }
+        return rows
+    }
+
+    private footer(): any {
         const footer = {
             type: 'none',
-            margin: [41, -40, 0, 0],
+            margin: [41, -62, 0, 0],
             ul: [
-                { text: 'ΚΡΟΤΣΗΣ ΕΠΕ', fontSize: 8 },
-                { text: 'ΕΜΠΟΡΙΟ ΕΛΑΣΤΙΚΩΝ & ΖΑΝΤΩΝ', fontSize: 6 },
-                { text: 'ΑΦΜ: 099863549', fontSize: 6 },
-                { text: 'ΔΟΥ: ΚΕΡΚΥΡΑΣ', fontSize: 6 },
-                { text: 'ΕΘΝΙΚΗ ΟΔΟΣ ΚΕΡΚΥΡΑΣ - ΛΕΥΚΙΜΜΗΣ 17Α', fontSize: 6 },
-                { text: 'ΠΑΓΚΡΑΤΕΪΚΑ', fontSize: 6 },
-                { text: 'krotsis.elastika@hotmail.com', fontSize: 6 },
-                { text: '26610 22533', fontSize: 6 },
+                { text: 'ΚΡΟΤΣΗΣ ΕΠΕ', fontSize: 10, style: 'PFHandbookProBold' },
+                { text: 'ΕΜΠΟΡΙΟ ΕΛΑΣΤΙΚΩΝ & ΖΑΝΤΩΝ', fontSize: 10 },
+                { text: 'ΑΦΜ: 099863549', fontSize: 10 },
+                { text: 'ΔΟΥ: ΚΕΡΚΥΡΑΣ', fontSize: 10 },
+                { text: 'ΕΘΝΙΚΗ ΟΔΟΣ ΚΕΡΚΥΡΑΣ - ΛΕΥΚΙΜΜΗΣ 17Α', fontSize: 10 },
+                { text: 'ΠΑΓΚΡΑΤΕΪΚΑ', fontSize: 10 },
+                { text: 'krotsis.elastika@hotmail.com', fontSize: 10 },
+                { text: '26610 22533', fontSize: 10 },
             ]
         }
         return footer
